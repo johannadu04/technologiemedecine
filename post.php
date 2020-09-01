fbfdfb4c89717da53b7b59489428ff05c6133605
Johanna Alètheia <tchjohanna@gmail.com>
2020-09-01 16:06:50 +0200

    derniere modif des post

--- post.php
+++ post.php
@@ -1,13 +1,20 @@
 <?php
-session_start();
 require('config/config.php');
 require('lib/database.lib.php');
 require('lib/app.lib.php');
 
-$titlePage = 'Nos dernières actualités';
+/** Les variables qui servent au layout et à la vue ! */
+$titlePage = '';
 $subTitlePage = '';
-$picturePage = 'img/home-bg.jpg';
-$view = 'category';
+$picturePage = '';
+$metaPage = true;
+$view = 'post';
+
+
+$nameComment = '';
+$emailComment = '';
+$contentComment = '';
+
 
 try {
     /** Si on a pas d'id fourni on lance une exception DomainException 
@@ -16,43 +23,32 @@ try {
     if (!array_key_exists('id', $_GET))
         throw new DomainException('Accès à la page non autorisé !');
 
-    /** On récupère la catégorie dans la base pour voir si elle existe */
+    /** On récupère l'article dans la base */
     $dbh = connect();
-    $sth = $dbh->prepare('SELECT cat_id, cat_title
-                        FROM b_categorie 
-                        WHERE cat_id = :idCategory');
-    $sth->bindValue('idCategory', $_GET['id']);
-    $sth->execute();
-    $category = $sth->fetch();
-
-
-
-    /** Si la catégorie n'est pas trouvé on lance une exception DomainException
-     * Le reste du code dans le try ne sera pas executé !
-     */
-    if ($category == false)
-        throw new DomainException('Cet catégorie n\'existe pas !');
-
-    /** On modifie le sous titre de la page pour y mettre le nom de la catégorie */
-    $subTitlePage .= $category['cat_title'];
-
-    /** Si tout est bon on récupère tous les articles de cette catégorie */
-    $sth = $dbh->prepare('SELECT *,use_firstname,use_lastname,cat_title
+    $sth = $dbh->prepare('SELECT b_article.*,use_firstname,use_lastname,cat_title
                         FROM b_article
                         INNER JOIN b_user ON (use_id = art_author)
                         INNER JOIN b_categorie ON (cat_id = art_categorie)
-                        WHERE art_categorie = :idCategory AND art_valide = 1 AND art_date_published <= NOW()
+                        WHERE art_id = :idArticle AND art_valide = 1 AND art_date_published <= NOW()
                         ORDER BY art_date_published DESC');
-    $sth->bindValue('idCategory', $_GET['id']);
+    $sth->bindValue('idArticle', $_GET['id']);
     $sth->execute();
-    $articles = $sth->fetchAll();
+    $article = $sth->fetch();
 
+    /** Si l'article n'est pas trouvé on lance une exception DomainException
+     * Le reste du code dans le try ne sera pas executé !
+     */
+    if ($article === false)
+        throw new DomainException('L\'article demandé n\'existe pas !');
 
+    // On modifie les données du jeu d'enregistrement pour le résumé de l'article et la date
+    $article['art_date_published'] = (new DateTime($article['art_date_published']))->format('d/m/Y');
 
-    foreach ($articles as $index => $article) {
-        $articles[$index]['art_content'] = mb_strimwidth(str_replace(['&eacute;', '&egrave;', '&rsquo;'], ['é', 'è', "'"], strip_tags($article['art_content'])), 0, RESUME_LENGTH, '...');
-        $articles[$index]['art_date_published'] = (new DateTime($article['art_date_published']))->format('d/m/Y');
-    }
+    // On modifie les variables de templates en fonction de l'article
+    $titlePage = $article['art_title'];
+    $picturePage = UPLOADS_URL . 'articles/' . $article['art_picture'];
+    $metaPage = 'Posté par <a href="author.php?id=' . $article['art_author'] . '">' . $article['use_firstname'] . ' ' . $article['use_lastname'] . '</a>
+                    le ' . $article['art_date_published'] . '- Dans <a href="category.php?id=' . $article['art_categorie'] . '">' . $article['cat_title'] . '</a>';
 } catch (PDOException $e) {
     /** ERREUR base de données 
      * On affiche simplement une page d'erreur simple pour l'internaute
@@ -64,6 +60,7 @@ try {
      * On renvoi une page avec un code 404 dans l'entête / Page non trouvée
      * Cela sert au référencement et éventuellement si un utilisateur arrive ici alors qu'un article a été supprimer
      * Le mieux dans cette page c'est de lui permettre de naviguer ou de rechercher dans le contenu du blog
+     * 
      */
     header("HTTP/1.0 404 Not Found");
     $titlePage = '404 Pas trouvé !';
